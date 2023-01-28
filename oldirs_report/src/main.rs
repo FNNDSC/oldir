b54c6d7facbf54841198a8a53d1bc27d09ea49b9
Jennings Zhang <Jennings.Zhang@childrens.harvard.edu>
2023-01-28 13:32:45 -0500

    Add grouping

--- oldirs_report/src/main.rs
+++ oldirs_report/src/main.rs
@@ -1,8 +1,11 @@
+mod prefix_buffer;
+
 use clap::Parser;
 use owo_colors::OwoColorize;
 use std::io::{self, BufRead};
 use ubyte::{ByteUnit, ToByteUnit};
 use users::Users;
+use crate::prefix_buffer::ParentPrintBuffer;
 
 #[derive(Parser)]
 #[clap(about = "Filter, pretty-print, and summarize the output of oldirs.")]
@@ -13,7 +16,66 @@ struct Cli {
     /// minimum size to filter by
     #[clap(short, long, default_value = "0B", value_parser = parse_byte_unit)]
     size: ByteUnit,
+
+    /// Aggregate paths which have common parent directories
+    /// if this many of them are seen in a row.
+    ///
+    /// Considering how `oldir` works, some of the files in
+    /// a grouped parent directory are not going to be older
+    /// than the `--since` argument given to `oldir`.
+    /// Nonetheless, a grouped directory probably contains
+    /// mostly old files which should all be reviewed.
+    ///
+    /// This functionality is "lossy," pass 0 to disable.
+    #[clap(short, long, default_value_t = 10)]
+    group: usize
 }
+
+fn main() -> anyhow::Result<()> {
+    let args: Cli = Cli::parse();
+    let mut total_size = ByteUnit::Byte(0);
+    let uc = users::UsersCache::new();
+    let mut printer = ParentPrintBuffer::new(args.group);
+
+    for line in io::stdin().lock().lines() {
+        let (path, uid, size) = line
+            .map_err(anyhow::Error::from)
+            .and_then(parse_oldirs_line)?;
+
+        // filters
+        if size < args.size {
+            continue;
+        }
+        if let Some(ref user) = args.user {
+            if user.uid() != uid {
+                continue;
+            }
+        }
+
+        total_size += size;
+
+        let username = if args.user.is_none() {
+            let colored_username = uc
+                .get_user_by_uid(uid)
+                .map(|u| u.name().to_string_lossy().cyan().to_string())
+                .unwrap_or_else(|| uid.magenta().to_string());
+            Some(colored_username)
+        } else {
+            None
+        };
+        printer.push(path, username, size);
+    }
+    printer.flush(args.user.map(|u| u.name().to_string_lossy().to_string()));
+
+    println!(
+        "        ============================== TOTAL SIZE: {} \
+                 ==============================",
+        total_size.bytes().yellow()
+    );
+    Ok(())
+}
+
+
 
 fn parse_byte_unit(s: &str) -> Result<ByteUnit, String> {
     s.parse().map_err(|e: ubyte::Error| e.to_string())
@@ -39,43 +101,3 @@ fn parse_user(given_user: &str) -> Result<users::User, String> {
     }
 }
 
-fn main() -> anyhow::Result<()> {
-    let args: Cli = Cli::parse();
-    let mut total_size = ByteUnit::Byte(0);
-    let uc = users::UsersCache::new();
-
-    for line in io::stdin().lock().lines() {
-        let (path, uid, size) = line
-            .map_err(anyhow::Error::from)
-            .and_then(parse_oldirs_line)?;
-
-        // filters
-        if size < args.size {
-            continue;
-        }
-        if let Some(ref user) = args.user {
-            if user.uid() != uid {
-                continue;
-            }
-        }
-
-        total_size += size;
-
-        if args.user.is_none() {
-            let colored_username = uc
-                .get_user_by_uid(uid)
-                .map(|u| u.name().to_string_lossy().cyan().to_string())
-                .unwrap_or_else(|| uid.magenta().to_string());
-            println!("{} {} {}", path, colored_username, size.yellow())
-        } else {
-            println!("{} {}", path, size.yellow())
-        }
-    }
-
-    println!(
-        "        ============================== TOTAL SIZE: {} \
-                 ==============================",
-        total_size.bytes().yellow()
-    );
-    Ok(())
-}
---
+++ oldirs_report/src/prefix_buffer.rs
@@ -0,0 +1,78 @@
+use std::fmt::Display;
+use owo_colors::OwoColorize;
+use ubyte::ByteUnit;
+
+#[derive(Default)]
+pub(crate) struct ParentPrintBuffer {
+    // invariant: if parent is not empty, then buffer is not empty either.
+    parent: String,
+    buffer: Vec<(String, Option<String>, ByteUnit)>,
+    group: usize
+}
+
+impl ParentPrintBuffer {
+    pub fn new(group: usize) -> Self {
+        Self {
+            group,
+            ..Default::default()
+        }
+    }
+
+    pub fn push(&mut self, path: String, username: Option<String>, size: ByteUnit) {
+        if self.group == 0 {
+            print(&path, username, size, "");
+            return
+        }
+        let given_parent = parent_of(&path).to_string();
+        if self.parent.is_empty() {
+            self.parent = given_parent;
+            self.buffer.push((path, username, size));
+            return;
+        }
+        if self.parent == given_parent {
+            self.buffer.push((path, username, size))
+        } else {
+            self.flush(username)
+        }
+    }
+
+    pub fn flush(&mut self, username: Option<String>) {
+        if self.group == 0 {
+            self.print_all_and_drain();
+            return;
+        }
+        if self.buffer.len() >= self.group {
+            print(&self.parent, username, self.total_size(), " (grouped)");
+            self.parent.clear();
+            self.buffer.clear();
+        } else {
+            self.print_all_and_drain()
+        }
+    }
+
+    fn total_size(&self) -> ByteUnit {
+        self.buffer.iter().map(|(_p, _u, s)| s.clone()).fold(ByteUnit::Byte(0), |acc, x| acc + x)
+    }
+
+    fn print_all_and_drain(&mut self) {
+        self.parent.clear();
+        for (path, username, size) in self.buffer.drain(..) {
+            print(&path, username, size, "")
+        }
+    }
+}
+
+fn parent_of(path: &str) -> &str {
+    path.rsplit_once('/')
+        .map(|(parent, _after)| parent)
+        .unwrap_or(path)
+}
+
+fn print(path: impl Display, username: Option<String>, size: ByteUnit, suffix: impl Display) {
+    let suffix = suffix.dimmed();
+    if let Some(user) = username {
+        println!("{path} {user} {size}{suffix}")
+    } else {
+        println!("{path} {size}{suffix}")
+    }
+}
